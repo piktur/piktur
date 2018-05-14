da13e15220ec12aa32c54ca192a8b209d029ad8e
Daniel Small <piktur.io@gmail.com>
2018-05-15 00:49:47 +1000

    Utilise shared spec configuration
    See piktur_spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
-require 'rspec/core'
+# frozen_string_literal: true
 
-spec_helper = Dir[File.join(Gem.loaded_specs['rspec-core'].gem_dir, '**/spec_helper.rb')][0]
-require_relative spec_helper
-
-RSpec.configure do |config|
-  config.default_formatter = 'doc' if config.files_to_run.one?
-end
-
-require 'pry'
+require 'piktur/spec/spec_helper'
