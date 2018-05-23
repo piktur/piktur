23f74d5ad0d0d4ed87f7c1a2d0aadb58b43204fc
Daniel Small <piktur.io@gmail.com>
2018-05-23 23:54:52 +1000

    Fix test helpers

---
+++ spec/dummy/config/boot.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+ENV['BUNDLE_GEMFILE'] ||= File.join(Dir.pwd, 'Gemfile')
+
+require 'bundler/setup'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
+APP_ROOT = File.expand_path('dummy', __dir__)
+
 require 'piktur/spec/spec_helper'
+
+Piktur::Spec.init_coverage_reporting!
