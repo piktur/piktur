4a021fb3dcb15816828e61e6e639f618a984f8f2
Daniel Small <piktur.io@gmail.com>
2018-05-30 01:52:20 +1000

    Refactor Enum
    Build instances of Enum rather than subclasses

--- lib/piktur/support/enum.rb
+++ lib/piktur/support/enum.rb
@@ -6,66 +6,21 @@ module Piktur
 
   module Support
 
-    # @example Performance of enumerated collection
-    # ```
-    #   require 'benchmark'
-    #   require 'benchmark/ips'
-    #
-    #   n = 1_000_000
-    #
-    #   male = ::Genders[:male]
-    #
-    #   Benchmark.bmbm do |x|
-    #     x.report('local var cache') { n.times { male } }
-    #     x.report('singleton method') { n.times { ::Genders.male(false) } }
-    #     x.report('find') { n.times { ::Genders[:male] } }
-    #   end
-    #
-    #   Benchmark.ips do |x|
-    #     x.report('local var cache') { male }
-    #     x.report('singleton method') { ::Genders.male(false) }
-    #     x.report('find') { ::Genders[:male] }
-    #
-    #     x.compare!
-    #   end
-    # ```
+    # Enum maps static values and translation to a developer friendly identifier.
     #
     # @example
-    #   class Example
-    #     ::Enum[
-    #       Module,
-    #       :collection,
-    #       predicates: 'collection',
-    #       a: { value: 0, default: true }
-    #       b: { value: 10 }
-    #       c: { value: 100 }
-    #     ]
+    #   class Genders < Enum
+    #     enum  :genders,
+    #           nil:        { value: 0, default: true },
+    #           male:       { value: 1 },
+    #           female:     { value: 2 },
+    #           non_binary: { value: 3 }
     #   end
     #
-    #   Example::Enum[0].value # => 0
-    #
-    # @param [Class, Module] namespace
-    # @param [Symbol] collection The collection name
-    # @param [Array<Symbol>] i18n_scope
-    # @param [String, Symbol] predicates Include {Predicates} for enumerated attribute
-    # @param [String, Symbol] scopes Include {Scopes} for enumerated attribute
-    # @param [Hash<Symbol=>Hash>] options Enumerated values and options
-    ::Enum = lambda do |namespace, collection, i18n_scope: nil, **options, &block|
-      i18n_scope ||= namespace unless namespace == Object
-      predicates_for, scopes_for = options.extract!(:predicates, :scopes).values
-      const = Inflector.camelize(collection)
-      enum  = ::Class.new(::Piktur::Support::Enum) do
-        enum(collection, i18n_scope: i18n_scope, **options, &block)
-      end
-
-      namespace.const_set(const, enum)
-
-      namespace.include enum.predicates(predicates_for) if predicates_for
-      namespace.include enum.scopes(scopes_for) if scopes_for
-      true
-    end
-
-    # Enum maps static values and translation to a developer friendly identifier.
+    #   Genders[1]          # => <Piktur::Support::Enum::Value key=:male value=1>
+    #   Genders[:male]      # => <Piktur::Support::Enum::Value key=:male value=1>
+    #   Genders.male        # => 1
+    #   Genders.male(false) # => <Piktur::Support::Enum::Value key=:male value=1>
     class Enum
 
       I18N_NAMESPACE = :enum
@@ -170,184 +125,255 @@ module Piktur
 
       end
 
-      # @example
-      #   class Genders < Enum
-      #     enum  :genders,
-      #           nil:        { value: 0, default: true },
-      #           male:       { value: 1 },
-      #           female:     { value: 2 },
-      #           non_binary: { value: 3 }
-      #   end
-      #
-      #   Genders[1]          # => <Piktur::Support::Enum::Value key=:male value=1>
-      #   Genders[:male]      # => <Piktur::Support::Enum::Value key=:male value=1>
-      #   Genders.male        # => 1
-      #   Genders.male(false) # => <Piktur::Support::Enum::Value key=:male value=1>
       class << self
 
-        attr_reader :collection, :mapping, :keys, :values, :default
-
-        # @param [Symbol] collection
+        # @example Performance of enumerated collection
+        # ```
+        #   require 'benchmark'
+        #   require 'benchmark/ips'
+        #
+        #   n = 1_000_000
+        #
+        #   male = ::Genders[:male]
+        #
+        #   Benchmark.bmbm do |x|
+        #     x.report('local var cache') { n.times { male } }
+        #     x.report('singleton method') { n.times { ::Genders.male(false) } }
+        #     x.report('find') { n.times { ::Genders[:male] } }
+        #   end
+        #
+        #   Benchmark.ips do |x|
+        #     x.report('local var cache') { male }
+        #     x.report('singleton method') { ::Genders.male(false) }
+        #     x.report('find') { ::Genders[:male] }
+        #
+        #     x.compare!
+        #   end
+        # ```
+        #
+        # @example
+        #   class Example
+        #     ::Enum[
+        #       Module,
+        #       :collection,
+        #       predicates: 'collection',
+        #       a: { value: 0, default: true }
+        #       b: { value: 10 }
+        #       c: { value: 100 }
+        #     ]
+        #   end
+        #
+        #   Example::Enum[0].value # => 0
+        #
+        # @param [Class, Module] namespace
+        # @param [Symbol] collection The collection name
         # @param [Array<Symbol>] i18n_scope
-        # @param [Hash] enumerated
-        # @return [void]
-        def enum(collection, i18n_scope: nil, **enumerated)
-          @collection, @mapping = collection.to_sym, {}
-          @i18n_scope = _i18n_scope(i18n_scope)
-          enumerated.each { |key, options| declare!(key, i18n_scope: @i18n_scope, **options) }
-          @keys, @values = mapping.keys.freeze, mapping.values.freeze
-          @mapping.freeze
-          @default = @values.find { |e| e.default? }
-          @_key = @i18n_scope.join('.').tr('/', '.') # Piktur::Types.container key
-
-          register_type(@_key)
-
-          yield if block_given?
-
-          freeze
+        # @param [String, Symbol] predicates Include {Predicates} for enumerated attribute
+        # @param [String, Symbol] scopes Include {Scopes} for enumerated attribute
+        # @param [Hash<Symbol=>Hash>] options Enumerated values and options
+        # @return [Object] the immutable Enum instance
+        def call(namespace, collection, i18n_scope: nil, **options, &block)
+          i18n_scope ||= namespace unless namespace == Object
+          builders = options.extract!(:predicates, :scopes).values
+          enum = new(collection, i18n_scope: i18n_scope, **options, &block)
+          enum.send(:set, namespace, *builders)
+          enum.send(:register_type)
+          enum
         end
-
-        # @param [Symbol, String, Integer] input
-        # @return [Value]
-        def call(input)
-          find(input) || default
-        end
-
-        # @return [Dry::Types::Constructor]
-        def type
-          Types[@_key]
-        end
-
-        # @!method find
-        # @!method find_by_key
-        # @!method find_by_value
-        # @param [String, Symbol, Integer] value
-        # @return [Enum::Value]
-
-        def find(value); value && (find_by_value(value) || find_by_key(value)); end
-
-        def find!(value); find(value) || not_found!(value); end
-        alias [] find!
-
-        def find_by_key(key); values.find { |obj| obj == key }; end
-
-        def find_by_value(value); values.find { |obj| obj == value }; end
-
-        # @!method []
-        # @!method find!
-        # @!method find_by_key!
-        # @!method find_by_value!
-        # @param [Object] value
-        # @raise [ArgumentError]
-        # @return [Enum::Value]
-
-        def find_by_key!(key); key && find_by_key(key) || not_found!(value); end
-
-        def find_by_value!(value); value && find_by_value(value) || not_found!(value); end
-
-        # @return [Boolean]
-        def include?(value); find(value).present?; end
-
-        # @!attribute [r] default
-        # @return [Enum::Value]
-
-        # @return [Integer]
-        # @return [nil] if default value not set
-        def default_value; default&.value; end
-
-        # @return [Integer]
-        def size; values.size; end
-
-        # @return [Array]
-        def each(&block); mapping.each(&block); end
-
-        # @return [Array]
-        def select(&block); mapping.select(&block); end
-
-        # @param [Array<Symbol>] args
-        # @return [Array]
-        def values_at(*args); mapping.values_at(*args); end
-
-        # @return [Enumerator]
-        def to_enum; mapping.enum_for; end
-
-        # @return [Hash]
-        def to_hash; mapping.transform_values { |v| v.value }; end
-        alias to_h to_hash
-
-        # @!method to_a
-        #   @return [Array<Enum::Value>]
-        alias to_a values
-
-        # @return [String, Symbol] attribute
-        # @return [Module]
-        def predicates(attribute); Predicates[attribute, self]; end
-
-        # @return [String, Symbol] attribute
-        # @return [Module]
-        def scopes(attribute); Scopes[attribute, self]; end
-
-        private
-
-          # @!method key()
-          #   @return [Integer]
-          # @!method key(false)
-          #   @return [Value]
-
-          # Register coercer with Piktur::Types container. Coercer WILL return {Value}
-          # corresponding to input, otherwise default or nil.
-          #
-          # @param [String] key
-          # @return [Dry::Types::Constructor]
-          def register_type(key)
-            ::Piktur::Types
-              .register key,
-                        ::Dry::Types['object'].constructor { |input| call(input) },
-                        call:    false,
-                        memoize: false
-          end
-
-          # * Store {Value} under `key`
-          # * Define scoped `I18n` helper
-          # * Define method for `key`
-          # @return [void]
-          def declare!(key, value:, **options)
-            validate!((key = key.to_sym), value)
-            obj = mapping[key] = Value.new(key: key, value: value, **options)
-
-            return if key == :default # Prevent method override
-
-            warn "Name Collision: method #{@collection}.#{key} is already defined, #{__FILE__}:#{__LINE__}" if
-              singleton_class.method_defined?(key)
-
-            define_singleton_method(key) { |cast = true| cast ? obj.value : obj }
-          end
-
-          def not_found!(value)
-            raise ArgumentError, NOT_FOUND_MSG % { value: value, enum: @collection }
-          end
-
-          def validate!(key, value)
-            raise ArgumentError, DUPLICATE_KEY_MSG % { key: key } if
-              duplicate_key?(key)
-            raise ArgumentError, DUPLICATE_VALUE_MSG % { key: key, value: value } if
-              duplicate_value?(value)
-            true
-          end
-
-          def duplicate_key?(value); mapping.key?(value); end
-
-          def duplicate_value?(value); mapping.find { |_, obj| value == obj.value }; end
-
-          def _i18n_scope(namespace)
-            namespace ||= parent_name
-            if namespace && namespace != Object
-              namespace = Inflector.underscore(namespace.to_s).to_sym
-            end
-            [I18N_NAMESPACE, *namespace, @collection]
-          end
+        alias [] call
 
       end
+
+      attr_reader :collection, :mapping, :keys, :values
+
+      # @param [Symbol] collection
+      # @param [Array<Symbol>] i18n_scope
+      # @param [Hash] enumerable
+      # @yieldparam [Enum] enum
+      # @return [void]
+      def initialize(collection, i18n_scope: nil, **enumerable)
+        @collection, @mapping = collection.to_sym, {}
+        @i18n_scope = _i18n_scope(i18n_scope).freeze
+
+        map(enumerable)
+        default
+        const
+        key
+
+        yield(self) if block_given?
+
+        freeze
+      end
+
+      # @param [Symbol, String, Integer] input
+      # @return [Value]
+      def call(input)
+        find(input) || default
+      end
+
+      # @return [Dry::Types::Constructor]
+      def type
+        Types[@_key]
+      end
+
+      # @!method find
+      # @!method find_by_key
+      # @!method find_by_value
+      # @param [String, Symbol, Integer] value
+      # @return [Enum::Value]
+
+      def find(value); value && (find_by_value(value) || find_by_key(value)); end
+
+      def find!(value); find(value) || not_found!(value); end
+      alias [] find!
+
+      def find_by_key(key); values.find { |obj| obj == key }; end
+
+      def find_by_value(value); values.find { |obj| obj == value }; end
+
+      # @!method []
+      # @!method find!
+      # @!method find_by_key!
+      # @!method find_by_value!
+      # @param [Object] value
+      # @raise [ArgumentError]
+      # @return [Enum::Value]
+
+      def find_by_key!(key); key && find_by_key(key) || not_found!(value); end
+
+      def find_by_value!(value); value && find_by_value(value) || not_found!(value); end
+
+      # @return [Boolean]
+      def include?(value); find(value).present?; end
+
+      # @return [Enum::Value]
+      def default; @default ||= @values.find { |e| e.default? }; end
+
+      # @return [Integer]
+      # @return [nil] if default value not set
+      def default_value; default&.value; end
+
+      # @return [Integer]
+      def size; values.size; end
+
+      # @return [Array]
+      def each(&block); mapping.each(&block); end
+
+      # @return [Array]
+      def select(&block); mapping.select(&block); end
+
+      # @param [Array<Symbol>] args
+      # @return [Array]
+      def values_at(*args); mapping.values_at(*args); end
+
+      # @return [Enumerator]
+      def to_enum; mapping.enum_for; end
+
+      # @return [Hash]
+      def to_hash; mapping.transform_values { |v| v.value }; end
+      alias to_h to_hash
+
+      # @!method to_a
+      #   @return [Array<Enum::Value>]
+      alias to_a values
+
+      # @return [String, Symbol] attribute
+      # @return [Module]
+      def predicates(attribute); Predicates[attribute, self]; end
+
+      # @return [String, Symbol] attribute
+      # @return [Module]
+      def scopes(attribute); Scopes[attribute, self]; end
+
+      private
+
+        # Build {Value} for each in `enumerable` collection.
+        #
+        # @param [Hash] enumerable
+        # @return [void]
+        def map(enumerable)
+          enumerable.each { |key, options| declare!(key, i18n_scope: @i18n_scope, **options) }
+          @keys, @values = mapping.keys.freeze, mapping.values.freeze
+          @mapping.freeze
+        end
+
+        # @!method key()
+        #   @return [Integer]
+        # @!method key(false)
+        #   @return [Value]
+
+        def const; @_const ||= Inflector.camelize(@collection).freeze; end
+
+        def key; @_key ||= @i18n_scope.join('.').tr('/', '.').freeze; end
+
+        # Assign instance to `namespace`
+        #
+        # @param [Module] namespace
+        # @param [String, Symbol] predicates Include {Predicates} for enumerated attribute
+        # @param [String, Symbol] scopes Include {Scopes} for enumerated attribute
+        # @return [void]
+        def set(namespace, predicates = nil, scopes = nil)
+          namespace.const_set(@_const, self)
+
+          namespace.include(self.predicates(predicates)) if predicates
+          namespace.include(self.scopes(scopes)) if scopes
+        end
+
+        # Register coercer with Piktur::Types container. Coercer WILL return {Value}
+        # corresponding to input, otherwise default or nil.
+        #
+        # Coercer SHOULD BE used when presenting enumerated value for human consumption or when
+        # validating input.
+        #
+        # @param [String] key
+        # @return [Dry::Types::Constructor]
+        def register_type
+          ::Piktur::Types
+            .register @_key,
+                      ::Dry::Types['object'].constructor { |input| call(input) },
+                      call:    false,
+                      memoize: false
+        end
+
+        # * Store {Value} under `key`
+        # * Define scoped `I18n` helper
+        # * Define method for `key`
+        # @return [void]
+        def declare!(key, value:, **options)
+          validate!((key = key.to_sym), value)
+          obj = mapping[key] = Value.new(key: key, value: value, **options)
+
+          return if key == :default # Prevent method override
+
+          warn "Name Collision: method #{@collection}.#{key} is already defined, #{__FILE__}:#{__LINE__}" if
+            singleton_class.method_defined?(key)
+
+          define_singleton_method(key) { |cast = true| cast ? obj.value : obj }
+        end
+
+        def not_found!(value)
+          raise ArgumentError, NOT_FOUND_MSG % { value: value, enum: @collection }
+        end
+
+        def validate!(key, value)
+          raise ArgumentError, DUPLICATE_KEY_MSG % { key: key } if
+            duplicate_key?(key)
+          raise ArgumentError, DUPLICATE_VALUE_MSG % { key: key, value: value } if
+            duplicate_value?(value)
+          true
+        end
+
+        def duplicate_key?(value); mapping.key?(value); end
+
+        def duplicate_value?(value); mapping.find { |_, obj| value == obj.value }; end
+
+        def _i18n_scope(namespace)
+          namespace ||= self.class.parent_name
+          if namespace && namespace != Object
+            namespace = Inflector.underscore(namespace.to_s).to_sym
+          end
+          [I18N_NAMESPACE, *namespace, @collection]
+        end
 
       # Enumerated attribute predicates
       # @example
@@ -491,3 +517,5 @@ module Piktur
   end
 
 end
+
+Enum = Piktur::Support::Enum
