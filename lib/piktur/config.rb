d2f400271a34a94bee44f0868b7ec1cfbf34825b
Daniel Small <piktur.io@gmail.com>
2018-06-09 10:08:33 +1000

    Document #services

--- lib/piktur/config.rb
+++ lib/piktur/config.rb
@@ -10,10 +10,11 @@ module Piktur
     extend ::ActiveSupport::Autoload
     extend ::Dry::Configurable
 
-    # @!method services(%w(piktur_library piktur_engine piktur_application))
+    # @!method services
+    #   @example Piktur.config.services = %w(piktur_library piktur_engine piktur_application)
     #   @see Piktur::Services
     #   @return [Services::Index]
-    setting(:services, reader: true) { |dependencies| Services::Index.new(*dependencies) }
+    setting(:services, reader: true) { |services| Services::Index.new(*services) }
 
   end
 
