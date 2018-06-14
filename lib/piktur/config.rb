cd1f2ffc7372162f20f4a16594263ded259e321c
Daniel Small <piktur.io@gmail.com>
2018-06-15 09:56:16 +1000

    Config services accepts an options Hash

--- lib/piktur/config.rb
+++ lib/piktur/config.rb
@@ -11,10 +11,20 @@ module Piktur
     extend ::Dry::Configurable
 
     # @!method services
-    #   @example Piktur.config.services = %w(piktur_library piktur_engine piktur_application)
+    #   @example
+    #     Piktur::Config.configure do |config|
+    #       config.services = %w(piktur_library piktur_engine piktur_application)
+    #
+    #       # With options
+    #       config.services = %w(lib).push(component_types: [:models, :serializers])
+    #     end
+    #
     #   @see Piktur::Services
     #   @return [Services::Index]
-    setting(:services, reader: true) { |services| Services::Index.new(*services) }
+    setting(:services, reader: true) do |services|
+      options = services.pop if services && services[-1].is_a?(::Hash)
+      Services::Index.new(services.map(&:to_s), options || EMPTY_HASH)
+    end
 
   end
 
