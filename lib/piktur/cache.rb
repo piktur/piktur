86a4393b4b3555c81a574e42e6db698da6089d34
Daniel Small <piktur.io@gmail.com>
2018-06-15 10:07:11 +1000

    Improve documentation

--- lib/piktur/cache.rb
+++ lib/piktur/cache.rb
@@ -2,13 +2,14 @@
 
 module Piktur
 
-  # Provides temporary storage containers
+  # Provides a thread safe memory store with `Hash` like interface.
+  #
   # @see file:spec/cache_spec.rb
   module Cache
 
     # Provides multi value read capability equivalent to
-    #   * [Hash#fetch_values](//ruby-doc.org/core-2.4.1/Hash.html#method-i-fetch_values)
-    #   * [Hash#values_at](//ruby-doc.org/core-2.4.1/Hash.html#method-i-values_at)
+    #   * [Hash#fetch_values](http://ruby-doc.org/core-2.4.1/Hash.html#method-i-fetch_values)
+    #   * [Hash#values_at](http://ruby-doc.org/core-2.4.1/Hash.html#method-i-values_at)
     #
     module ReadMulti
 
