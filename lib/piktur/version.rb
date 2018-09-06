17b7f0cccf709a0350b7836a7ecdbde42b3778ec
Daniel Small <piktur.io@gmail.com>
2018-09-06 22:28:47 +1000

    🔧 config: set default RAILS_VERSION

--- lib/piktur/version.rb
+++ lib/piktur/version.rb
@@ -4,7 +4,7 @@ module Piktur
 
   VERSION = '0.0.1'
 
-  def self.rails_version; ENV.fetch('RAILS_VERSION'); end
+  def self.rails_version; ENV.fetch('RAILS_VERSION') { '5.1' }; end
 
   def self.ruby_version; ENV.fetch('RUBY_VERSION').sub('ruby-', ''); end
 
