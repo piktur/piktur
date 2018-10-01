560b3ad0177e486e3aa1ccfb7a67ff2a3318274c
Daniel Small <piktur.io@gmail.com>
2018-10-01 11:20:38 +1000

    👕 refactor: Piktur::Spec application helpers

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 require 'piktur/spec'
-Piktur::Spec.define_test_application!
+Piktur::Spec.define_rails_application!
--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 require 'piktur/spec'
-Piktur::Spec.init_test_application!
+Piktur::Spec.init_rails_application!
