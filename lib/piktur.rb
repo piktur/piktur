ac1fe6cb80b7a0c9d77d3797a32f465d1eecc2db
Daniel Small <piktur.io@gmail.com>
2018-10-01 10:05:19 +1000

    ⚡️ improvement: isolate environment logic

--- lib/piktur.rb
+++ lib/piktur.rb
@@ -87,6 +87,32 @@ module Piktur
     # @return [Services::Service]
     def application; services.application; end
 
+    # Predicate checks existence of Rails application singleton. Use when opting out of operations
+    # that will be handled by the Rails boot.
+    #
+    # @return [Boolean]
+    def rails?
+      defined?(::Rails) && ::Rails.application.present? # &.initialized?
+    end
+
+    # @return [Boolean]
+    def initialized?
+      rails? && ::Rails.application.initialized?
+    end
+
+    # @return [Boolean]
+    def rake?
+      defined?(::Rake) && ::Rake.application.present?
+    end
+
+    # Predicate checks Rails application singleton is an instance of the dummy application.
+    #
+    # @return [Boolean]
+    def dummy?
+      defined?(::Piktur::Spec::Application) &&
+        ::Rails.application.is_a?(::Piktur::Spec::Application)
+    end
+
     # @return [Array<Services::Application>]
     def applications; services.applications; end
 
