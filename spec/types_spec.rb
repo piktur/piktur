ca4a36940171badeee54fcaaf1b1718325102fa9
Daniel Small <piktur.io@gmail.com>
2018-10-01 11:12:35 +1000

    💚 test: types

--- spec/types_spec.rb
+++ spec/types_spec.rb
@@ -1,60 +1,54 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-
 require 'piktur/spec/helpers/container'
 
-require 'piktur/support/enum'
-require 'piktur/support/types'
+RSpec.require_support 'container', app: 'piktur'
 
 module Piktur
   module Support
 
     RSpec.describe Types do
-      it_should_behave_like 'a container', Types.container
+      include_context 'container'
+      it_should_behave_like 'a container'
 
-      Spec::Helpers::Container.containerize(Types, stub: true)
+      subject { Types.container }
 
       describe '.Model(key, constructor)' do
-        include_context 'stub container', Types
-
         before(:all) do
-          ::Model = ::Class.new do
-            def self.name
-              'HisDisgust'
-            end
-
-            def self.call(params)
-              new(params)
-            end
-          end
+          ::Test.safe_const_set(:Model, ::Class.new {
+            attr_reader :attributes
+            def self.call(params); new(params); end
+            def initialize(input); @attribues = input; end
+          })
         end
 
-        let(:key)         { Inflector.underscore(Model.name) }
-        let(:constructor) { Model.method(:call).to_proc }
-        let(:result)      { Types.container[key] }
+        after(:all) { ::Test.safe_remove_const(:Model) }
+
+        before do
+          reset_container!(:container, namespace: Types, stub: false)
+          Types.Model(key, ::Test::Model)
+        end
+
+        let(:key) { Inflector.underscore(::Test::Model.name) }
+        let(:result) { Types.container[key] }
 
         describe 'the constructor' do
-          before do
-            Types.Model(key, constructor)
+          it 'is assigned to the Types container' do
+            expect(result).to be_a(::Dry::Types::Type)
           end
 
-          it 'is assigned to the Types container' do
-            expect(result).to be_a(::Proc)
+          it 'builds a new class' do
+            expect(result.call(blackened: :sheath))
           end
         end
       end
 
       describe '.Enum(name, namespace, options, &block)' do
-        include_context 'stub container', Types
+        before do
+          reset_container!(:container, namespace: Types, stub: false)
 
-        let(:options)   { ::Hash[predicates: nil, i18n_scope: nil] }
-        let(:name)      { :colours }
-        let(:namespace) { ::Test.safe_const_reset(:Palette, ::Module.new) }
-        let(:key)       { 'enum.test.palette.colours' }
-
-        let(:block) do
-          lambda do
+          Types.Enum(name, namespace: namespace, **options) do
             default :black
             value   :blue
             value   :purple
@@ -62,15 +56,18 @@ module Piktur
           end
         end
 
-        before do
-          Types.Enum(name, namespace: namespace, **options, &block)
-        end
-
+        let(:options) { ::Hash[predicates: nil, i18n_scope: nil] }
+        let(:name) { :colours }
+        let(:namespace) { ::Test.safe_const_reset(:Palette, ::Module.new) }
+        let(:key) { 'enum.test.palette.colours' }
         let(:result) { Types.container[key] }
 
         describe 'the constructor' do
           it 'is assigned to the Types container' do
-            expect(result).to be_a(Proc)
+            expect(result).to be_a(::Dry::Types::Type)
+          end
+
+          it 'casts input to enum value' do
             expect(result[:black]).to be_a(Enum::Value).or(raise_error)
           end
         end
