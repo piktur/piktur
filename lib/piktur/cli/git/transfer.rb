adf8758a29026466465b518ce3c9d3064fa73bb3
Daniel Small <piktur.io@gmail.com>
2018-10-31 16:22:02 +1100

    👕 refactor: git transfer

--- lib/piktur/cli/git/transfer.rb
+++ lib/piktur/cli/git/transfer.rb
@@ -7,29 +7,27 @@
 
 require 'optparse'
 
-from, to, regex = nil
+from, to, pattern = nil
 
 OptionParser.new do |opts|
   opts.on('--from=MANDATORY') { |val| from = File.expand_path(val, Dir.pwd) }
   opts.on('--to=MANDATORY') { |val| to = File.expand_path(val, Dir.pwd) }
-  opts.on('--regex=MANDATORY') { |val| regex = Regexp.new(val) }
+  opts.on('--pattern=MANDATORY') { |val| pattern = val }
 end.parse!
 
 Dir.chdir(from) do
-  list = Dir['**/*.{rb,js}']
-    .reject { |e| e.start_with?('node_modules', 'coverage') }
-    .select { |e| e.match?(regex) }
+  list = Dir[pattern]
 
   File.open('list', 'w+') do |f|
     list.each { |e| f.write e << "\n" }
   end
 
-  `git log
-  --pretty=email \
-  --patch-with-stat \
-  --reverse \
-  --full-index \
-  --binary -- $(cat list) > patch`
+  `git log \
+--pretty=email \
+--patch-with-stat \
+--reverse \
+--full-index \
+--binary -- $(cat list) > patch`
 end
 
 Dir.chdir(to) do
