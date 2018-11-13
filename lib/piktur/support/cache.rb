4802864139c30d73f3445f4cbbacea1d4b91f033
Daniel Small <piktur.io@gmail.com>
2018-11-13 14:27:44 +1100

    👕 refactor: module nesting

--- lib/piktur/support/cache.rb
+++ lib/piktur/support/cache.rb
@@ -2,44 +2,42 @@
 
 require 'dry/core/cache'
 
-module Piktur
+# rubocop:disable ClassAndModuleChildren
 
-  module Support
+module Piktur::Support
 
-    # Provides a thread safe memory store with `Hash` like interface.
+  # Provides a thread safe memory store with `Hash` like interface.
+  #
+  # @see file:spec/cache_spec.rb
+  module Cache
+
+    # Provides multi value read capability equivalent to
+    #   * [Hash#fetch_values](http://ruby-doc.org/core-2.4.1/Hash.html#method-i-fetch_values)
+    #   * [Hash#values_at](http://ruby-doc.org/core-2.4.1/Hash.html#method-i-values_at)
     #
-    # @see file:spec/cache_spec.rb
-    module Cache
+    module ReadMulti
 
-      # Provides multi value read capability equivalent to
-      #   * [Hash#fetch_values](http://ruby-doc.org/core-2.4.1/Hash.html#method-i-fetch_values)
-      #   * [Hash#values_at](http://ruby-doc.org/core-2.4.1/Hash.html#method-i-values_at)
-      #
-      module ReadMulti
+      # @raise [KeyError]
+      # @return [Object]
+      def fetch_values(*args)
+        args.map! { |key| fetch(key) }
+      end
 
-        # @raise [KeyError]
-        # @return [Object]
-        def fetch_values(*args)
-          args.map! { |key| fetch(key) }
-        end
-
-        # @return [Object]
-        def values_at(*args)
-          args.map! { |key| self[key] }
-        end
-
+      # @return [Object]
+      def values_at(*args)
+        args.map! { |key| self[key] }
       end
 
     end
 
   end
 
-  # A **thread safe** data store caching the return value of a given block by the given arguments.
-  class Cache
+end
 
-    extend  ::Dry::Core::Cache
-    include Support::Cache::ReadMulti
+# A **thread safe** data store caching the return value of a given block by the given arguments.
+class Piktur::Cache
 
-  end
+  extend  ::Dry::Core::Cache
+  include ::Piktur::Support::Cache::ReadMulti
 
 end
